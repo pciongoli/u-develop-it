abd78aad4971b11c3ee9bb0f25d492a95e9e7f85
pcion <patrick.ciongoli@gmail.com>
2022-03-18 15:25:31 -0400

    set up server.js

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 const express = require('express');
-// port designation
+
+// port designation and app designation
 const PORT = process.env.PORT || 3001;
-// app expression
 const app = express();
 
-
-
-// express.js middleware
+// Express.js middleware
 app.use(express.urlencoded({ extended: false }));
-
-
-// GET test route. Route method get() response method res.json()
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello'
-    });
-});
+app.use(express.json());
 
 // handle user requests that arent supported by the app
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 // function to start the express server on port 3001
 app.listen(PORT, () => {
