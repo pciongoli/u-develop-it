bb2b2db22252ca6cd20e14dc71e59f696d19eea4
pcion <patrick.ciongoli@gmail.com>
2022-03-18 17:14:45 -0400

    npm start now returning array from candidates table

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// import mysql2 package
+const mysql = require('mysql2');
 const express = require('express');
 
 // port designation and app designation
@@ -8,7 +10,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// handle user requests that arent supported by the app
+// connect the application to MYSQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// query the database to test the connection
+// rows is the database query response 
+// key component that allows SQL commands to be written in Node application
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// handle user requests that arent supported by the app catchall route
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
