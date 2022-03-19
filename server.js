74fc00c9c6cc4a0f911aebd5577757a305b9a875
pcion <patrick.ciongoli@gmail.com>
2022-03-18 20:07:17 -0400

    updated Delete a candidate

--- server.js
+++ server.js
@@ -63,12 +63,29 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Delete a candidate
 // statement has "?" to denote a placeholder
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+
 
 // Create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
