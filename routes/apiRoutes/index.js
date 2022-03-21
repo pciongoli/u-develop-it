07c6e20fe28d95747c5aa32bfa7090f3bc15c7e4
pcion <patrick.ciongoli@gmail.com>
2022-03-21 18:43:34 -0400

    created an API route to add new votes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// make sure to tell router object to use each of the routes we created in routes/apiRoutes folder
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
